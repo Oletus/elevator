683e2cfd57fb545719a5075785162e03d34d8631
Kimmo Keskinen <keskinenkimmo@gmail.com>
2015-12-12 15:53:01 +0200

    Split floor.js into its own file and add loading to index.html

--- src/level.js
+++ src/level.js
@@ -1,16 +1,4 @@
 
-var FloorTiles = [
-    'xxxxxxxxxxxxxxxxxxxxxxh        h',
-    '                      h        h',
-    '                      d        h',
-    '                      d        h',
-    '                      d        h',
-    '                      d        h',
-    '                      d        h',
-    'xxxxxxxxxxxxxxxxxxxxxxh        h',
-    'xxxxxxxxxxxxxxxxxxxxxxh        h'
-];
-
 var ElevatorTiles = [
     'oooooooo',
     'o      o',
@@ -21,51 +9,6 @@ var ElevatorTiles = [
     'd      o',
     'oooooooo'
 ];
-
-var Floor = function(options) {
-    var defaults = {
-        floor: 0, // Floor number rises upwards
-        name: 'Products',
-        elevator: null,
-        level: null
-    };
-    objectUtil.initWithDefaults(this, defaults, options);
-    this.tilemap = new TileMap({initTile: TileMap.initFromData(FloorTiles), height: FloorTiles.length, width: FloorTiles[0].length });
-};
-
-Floor.prototype.render = function(ctx) {
-    ctx.save();
-    var drawY = this.level.getFloorTopY(this.floor);
-    ctx.translate(0, drawY);
-    ctx.fillStyle = '#222';
-    this.tilemap.render(ctx, function(tile) { return tile === 'x'; }, 0.05, 0.05);
-    ctx.fillStyle = '#888';
-    this.tilemap.render(ctx, function(tile) { return tile === 'h'; }, 0.05, 0.05);
-    ctx.globalAlpha = this.doorVisual;
-    ctx.fillStyle = '#da4';
-    this.tilemap.render(ctx, function(tile) { return tile === 'd'; }, 0.05, 0.05);
-    
-    ctx.globalAlpha = 1;
-    ctx.translate(21, 3);
-    ctx.scale(0.1, 0.1);
-    ctx.fillStyle = 'black';
-    var originalFont = ctx.font;
-    ctx.textAlign = "right";
-    ctx.font = "12px sans-serif";
-    ctx.fillText(this.name, 0, 0);
-    ctx.font = originalFont;
-    
-    ctx.restore();
-};
-
-Floor.prototype.update = function(deltaTime) {
-    if (Math.round(this.elevator.floor) == this.floor) {
-        this.doorOpen = this.elevator.doorOpen;
-        this.doorVisual = this.elevator.doorVisual;
-    }
-};
-
-Floor.height = FloorTiles.length - 1;
 
 var Elevator = function(options) {
     var defaults = {
