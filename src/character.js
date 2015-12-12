ba9e73edd60daa6b74fc0b2c06c2c1f78c800ea3
Olli Etuaho <olli.etuaho@gmail.com>
2015-12-12 15:10:01 +0200

    More character drawing

--- src/character.js
+++ src/character.js
@@ -6,14 +6,20 @@ var Character = function(options) {
         floor: 0, // Floor number rises upwards
         x: 0,
         level: null,
-        goalFloor: 0
+        goalFloor: 0,
+        characterType: 'customer'
     };
     objectUtil.initWithDefaults(this, defaults, options);
     this.legsSprite = new AnimatedSpriteInstance(Character.legsAnimation);
+    this.bodySprite = Character.bodySprites[this.characterType];
+};
+
+Character.bodySprites = {
+    'customer': new Sprite('body-customer.png')
 };
 
 Character.legsAnimation = new AnimatedSprite({
-        'idle': [{src: 'legsidle.png', duration: 0}],
+        'idle': [{src: 'legs-idle.png', duration: 0}],
 },
 {
     durationMultiplier: 1000 / 60,
@@ -27,6 +33,7 @@ Character.prototype.render = function(ctx) {
     var scale = 1 / 20;
     var flip = 1;
     this.legsSprite.drawRotatedNonUniform(ctx, 0, -1, 0, scale * flip, scale);
+    this.bodySprite.drawRotatedNonUniform(ctx, 0, -2, 0, scale * flip, scale);
     ctx.restore();
 };
 
