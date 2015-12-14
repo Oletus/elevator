49b6f52c2ac6dcf795f1bb9b365ae6b7589f3020
Olli Etuaho <olli.etuaho@gmail.com>
2015-12-14 22:52:08 +0200

    Tweak graveyard spawns

--- src/gamedata.js
+++ src/gamedata.js
@@ -31,9 +31,10 @@ GameData.floors =
         id: "graveyard",
         name: "Graveyard",
         spawnIds: [
-            {id: "customer", chance: 3},
-            {id: "horse", chance: 1},
-            {id: "ghost", chance: 2}
+            {id: "customer", chance: 5},
+            {id: "ghost", chance: 5},
+            {id: "cat", chance: 2},
+            {id: "horse", chance: 1}
         ]
     },
     {
